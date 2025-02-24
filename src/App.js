d661a40c2b56488d1263e49c9efb79bb62a4cd1f
Camila Sayuri <tokubo.camila@gmail.com>
2025-02-24 16:56:38 -0300

    Instalação do Material UI e inicialização da utilização do componente de Card do Material UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Container} from "@mui/material";
 import Banner from './componentes/Banner/Banner';
 import Pokedex from './services/Pokedex';
 import Footer from './componentes/Footer';
@@ -5,16 +6,18 @@ import Footer from './componentes/Footer';
 function App() {
   return (
     <div className="App">
-      <Banner />
-      <div className='Section-personagens'>
-        <div className='App-container'>
-          <h2 className='App_title'>Saiba mais sobre o seu personagem favorito</h2>
-          <div className='App_personagens'>
-            <Pokedex />
-          </div>
-        </div> 
-      </div>
-      <Footer />
+      <Container maxWidth="false">
+        <Banner />
+        <div className='Section-personagens'>
+          <div className='App-container'>
+            <h2 className='App_title'>Saiba mais sobre o seu personagem favorito</h2>
+            <div className='App_pokemons'>
+              <Pokedex />
+            </div>
+          </div> 
+        </div>
+        <Footer />
+      </Container>
     </div>
   );
 }
