16792ccc707925148963e92930c988efbf401e39
Camila Sayuri <tokubo.camila@gmail.com>
2025-03-05 10:00:00 -0300

    Acrescentar as informações de cada Pokémon em cada card

--- src/componentes/PokemonListItem/index.js
+++ src/componentes/PokemonListItem/index.js
@@ -7,29 +7,74 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
-export default function PokemonListItem() {
+const PokemonListItem = (props) => {
+
+  const [pokemon, setPokemon] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    setLoading(true);
+    console.log('Oi')
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.pokeName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const pokeObj = {
+          image: data.sprites.front_default,
+          number: data.id,
+          name: data.name,
+          types: "",
+        };
+
+        for (let index = 0; index < data.types.length; index++) {
+          const name = data.types[index].type.name;
+          pokeObj.types += `${name}`;
+          if (index < data.types.length - 1) {
+            pokeObj.types += ', ';
+          }
+        }
+        setPokemon(pokeObj)
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Erro ao buscar o Pokémon:", error);
+        setLoading(false);
+      })
+  }, [props.pokeName]);
+
+  if (loading) {
+    return (
+      <div>Carregando...</div>
+    )
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image={pokemon.image}
+          alt='Imagem do pokémon'
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {pokemon.number}
           </Typography>
-          {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography> */}
+          <Typography gutterBottom variant="h5" component="div">
+            {pokemon.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {pokemon.types}
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
   );
 }
+
+export default PokemonListItem;
 /*
 const PokemonListItem = (props) => {
 
